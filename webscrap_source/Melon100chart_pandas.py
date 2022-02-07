3a42d04f60bb6ae0f2c31240374c8ca7190937c0
JAEWON <ghkd6535@gmail.com>
2022-02-07 16:49:49 +0900

    pandas dataframe slicing

--- webscrap_source/Melon100chart_pandas.py
+++ webscrap_source/Melon100chart_pandas.py
@@ -11,5 +11,38 @@ for song_detail in song_detail_list:
     series_obj = pd.Series(song_detail)
     # list <=> DataFrame
     song_df2 = song_df2.append(series_obj, ignore_index=True)
-print(song_df2.head())
+#print(song_df2.head())
+
+#song_file을 읽어서 Dataframe 객체를 생성하는 방법
+song_df = pd.read_json('data/songs.json')
+#print(type(song_df))
+#print(song_df.head()) #앞에 5 줄
+#print(song_df.tail()) #끝에 5줄
+# print('shape', song_df.shape)
+# print('columns', song_df.columns)
+# print('index', song_df.index)
+# print('values', type(song_df.values)) #2차원배열
+
+# 가수 컬럼 값별로 row counting
+#print(song_df['가수'].value_counts())
+
+# unique한(중복제거) 가수 컬럼의 값을 가져오기
+#song_df['가수'].unique()
+
+#indexing
+#print(song_df.loc[song_df['가수'] == '임영웅'])
+
+# 특정 행과 열을 선택하기
+# loc[], iloc[] 사용한다.
+# Slicing 을 사용해서 구간을 주어서 행과 열을 선택
+# [ ]
+# (list)를 사용해서 여러개의 행과 열을 선택
+# 조건식을 만족하는 행과 열을 선택
+
+# Slicing : 인덱스가 0부터 5까지의 행과 모든 열을 선택하기
+print(song_df.loc[0:5]) # song_df.loc[0:5,:])
+
+# Slicing : 인덱스가 0부터 10까지의 행과 모든 열을 선택하기 , 1개의 행을 skip
+print(song_df.loc[0:10:2]) # song_df.loc[0:10:2,:]
+
 
