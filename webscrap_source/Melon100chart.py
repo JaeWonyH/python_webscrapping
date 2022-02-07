84ded576a101baee67c1018353ea108f3f6f2fa8
JAEWON <ghkd6535@gmail.com>
2022-02-07 12:00:22 +0900

    add melon 100chart json file

--- webscrap_source/Melon100chart.py
+++ webscrap_source/Melon100chart.py
@@ -26,6 +26,8 @@ if res.ok:
     a_tags = soup.select("div#tb_list tr a[href*='playSong']")
     #print(a_tags)
     song_list = [] #100곡 담을 list
+
+    #100곡의 노래의 제목과 songID 추출해서 list에 저장하기
     for idx,a_tag in enumerate(a_tags,1):
         song_dict = {} #song 정보 담을 dict
         #<a href = "">노래제목<a>
@@ -41,9 +43,38 @@ if res.ok:
             song_dict['song_id'] =song_id
             song_detail_url = f'https://www.melon.com/song/detail.htm?songId={song_id}' #노래 상세정보 url
             song_dict['song_detail_url'] = song_detail_url
-            #print(song_id, song_title, song_detail_url)
-            print(song_dict)
+            #print(song_dict)
             song_list.append(song_dict)
     print(len(song_list))
+
+# 100곡의 노래의 상세정보를 추출해서 list와 dict에 저장해서 json 파일로 저장하기
+song_detail_list = []
+for idx,song in enumerate(song_list[:2],1):
+    #print(song['song_detail_url'])
+    #song 1곡의 상세정보를 저장할 dict
+    song_detail_dict = {}
+    song_detail_url = song['song_detail_url']
+    res = requests.get(song_detail_url, headers=req_header_dict)
+    #print(res.status_code)
+    if res.ok:
+        soup = BeautifulSoup(res.text , 'html.parser')
+        song_detail_dict['곡명'] = song['song_title']
+        if soup.select("a[href*='goArtistDetail'] span"):
+            song_detail_dict['가수'] = soup.select("a[href*='goArtistDetail'] span")[0].text
+        if soup.select("div.meta dd")[0] :
+            song_detail_dict['앨범'] = soup.select("div.meta dd")[0].text
+        #print(song_detail_dict)
+        song_detail_list.append(song_detail_dict)
+        song_id  =song['song_id']
+        #print(song_id)
+        like_url = f'https://www.melon.com/commonlike/getSongLike.json?contsIds={song_id}'
+        like_res = requests.get(like_url,headers=req_header_dict)
+        #print(like_url)
+        #print(like_res.status_code)
+        #좋아요 건수 ajax 통신 함으로 json파일로 받음
+        #print(like_res.json()['contsLike'][0]['SUMMCNT'])
+        song_detail_dict['좋아요'] = like_res.json()['contsLike'][0]['SUMMCNT']
+print(song_detail_list)
+
 
 
