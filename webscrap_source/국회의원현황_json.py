e0abc1af8833b6a6a1eb698fb92d7c6e47a45f6c
JAEWON <ghkd6535@gmail.com>
2022-02-09 15:30:56 +0900

    국회의원  detail_info 리스트 pandas 분석

---
+++ webscrap_source/국회의원현황_json.py
@@ -0,0 +1,6 @@
+import json
+
+from webscrap_source.국회의원현황 import member_detail_list
+
+with open('data/member.json','w') as file:
+    json.dump(member_detail_list,file)
---
+++ webscrap_source/국회의원현황_pandas.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+member_df = pd.read_json('data/member.json')
