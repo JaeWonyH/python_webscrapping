033221832ad9bc8c9960506d15d6434555a156cb
JAEWON <ghkd6535@gmail.com>
2022-02-09 14:39:23 +0900

    국회의원 detail_info 추출

--- webscrap_source/국회의원현황.py
+++ webscrap_source/국회의원현황.py
@@ -8,6 +8,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from urllib.parse import urljoin
 
 url = "https://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do"
 req_param_dict ={
@@ -26,4 +27,25 @@ if res.ok :
         if matched:
             member_id = matched.group(1)
         member_id_list.append(member_id) #국회의원 id append
+
+member_detail_list = []
+for idx, mem_id in enumerate(member_id_list[:5],1):
+    detail_url = f'https://www.assembly.go.kr/assm/memPop/memPopup.do?dept_cd={mem_id}' #상세정보 url
+    res = requests.get(detail_url)
+    if res.ok:
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        #1명의 정보를 저장할 dict 선언
+        member_detail_dict = {}
+
+        dt_list = [dt_tag.text for dt_tag in soup.select('dl.pro_detail dt')]
+        dd_list = []
+        for dd_tag in soup.select('dl.pro_detail dd'):
+            pattern = re.compile(f'[\n\r\t]') #특수문자 패턴
+            dd_text = pattern.sub('',dd_tag.text.strip()).replace(' ','') #dd_tag의 특수문자 empty string으로변환
+            dd_list.append(dd_text)
+        print(dd_list)
+
+
+
 
