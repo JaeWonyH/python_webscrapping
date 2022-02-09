dc78c16ac735d0259734e5302a3cb8e37a6b40dd
JAEWON <ghkd6535@gmail.com>
2022-02-09 14:15:16 +0900

    국회의원 id 추출

--- webscrap_source/국회의원현황.py
+++ webscrap_source/국회의원현황.py
@@ -9,4 +9,21 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "https://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do?currentPage=1&rowPerPage=500"
+url = "https://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do"
+req_param_dict ={
+    'currentPage':1,
+    'rowPerPage':500
+}
+res = requests.get(url,params=req_param_dict)
+
+if res.ok :
+    soup = BeautifulSoup(res.text, 'html.parser') #페이지 소스보기
+    atag_list = soup.select('div.memberna_list dl dt a')
+    member_id_list = [] #국회의원 id 번호 list
+    for atag in atag_list:
+        href = atag['href']
+        matched = re.search(r'(\d+)',href) #국회의원 id 추출
+        if matched:
+            member_id = matched.group(1)
+        member_id_list.append(member_id) #국회의원 id append
+
